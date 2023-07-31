3adaa9d22382454895dbab10695f54772d68d2ce
Lukasz710 <lubaszewskilukasz0@gmail.com>
2023-07-31 22:07:29 +0200

    small fix

--- E14/06-2019/index.php
+++ E14/06-2019/index.php
@@ -9,7 +9,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sklep papierniczy</title>
     <link rel="stylesheet" href="styl.css">
-    <title>Document</title>
 </head>
 <body>
     <section id="baner">
