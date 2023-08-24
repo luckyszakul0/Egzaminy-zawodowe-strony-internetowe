0ef709f2933944dde5852464ce4235e9518cd850
Lukasz710 <lubaszewskilukasz0@gmail.com>
2023-08-24 13:45:54 +0200

    finished the 01-2021 E14 exam

--- E14/01-2021/index.php
+++ E14/01-2021/index.php
@@ -21,6 +21,18 @@
         <h2>Lista aktualnie dostępnych podzespołów</h2>
         <table>
             <tr><th>NAZWA PODZESPOŁU</th><th>OPIS</th><th>CENA</th></tr>
+            <?php
+                $polaczenie = new mysqli('localhost', 'root', '', 'sklep');
+
+                $zapytanie1 = 'SELECT nazwa, opis, cena FROM podzespoly WHERE dostepnosc = 1;';
+                $wynik1 = $polaczenie->query($zapytanie1);
+
+                while($wiersz1 = $wynik1->fetch_assoc()){
+                    echo '<tr><td>' . $wiersz1['nazwa'] . '</td><td>' . $wiersz1['opis'] . '</td><td>' . $wiersz1['cena'] . '</td></tr>';
+                }
+
+                $polaczenie->close();
+            ?>
         </table>
     </main>
     <footer id="stopka-1">
