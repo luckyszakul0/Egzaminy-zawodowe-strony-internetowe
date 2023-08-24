354aece58f3e48869d10752df0a2a8640827b41d
Lukasz710 <lubaszewskilukasz0@gmail.com>
2023-08-24 13:27:59 +0200

    done some

---
+++ E14/01-2021/index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="pl-PL">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styl_1.css">
+    <title>Podzespoły komputerowe</title>
+</head>
+<body>
+    <header id="logo">
+        <h1>Sklep Komputerowy</h1>
+    </header>
+    <nav id="menu">
+        <a href="./index.php">Główna</a>
+        <a href="./procesory.html">Procesory</a>
+        <a href="./ram.html">RAM</a>
+        <a href="./grafika.html">Grafika</a>
+        <a href="./hdd.html">HDD</a>
+    </nav>
+    <main id="glowny">
+        <h2>Lista aktualnie dostępnych podzespołów</h2>
+        <table>
+            <tr><th>NAZWA PODZESPOŁU</th><th>OPIS</th><th>CENA</th></tr>
+        </table>
+    </main>
+    <footer id="stopka-1">
+        <h3>Sklep Komputerowy</h3>
+        <p>ul. Legnicka 61, Wrocław</p>
+        <p>Współpracujemy z hurtownią <a href="http://www.ddata.pl/" target="_blank">ddata</a></p>
+    </footer>
+    <footer id="stopka-2">
+        <p>Stronę wykonał: 00000000</p>
+    </footer>
+    <footer id="stopka-3">
+        <p>zadzwoń do nas: 71 506 50 60</p>
+    </footer>
+    <footer id="stopka-4">
+        <img src="./sklep.jpg" alt="sklep komputerowy">
+    </footer>
+</body>
+</html>
